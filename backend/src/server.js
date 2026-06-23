4792f3ce09165199006b4c97c65d1a46484179ba
mudit024 <jawaramudit1234@gmail.com>
2026-06-24 03:45:12 +0530

    Production setup completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,31 @@
 import express from "express"
+import path from "path";
+
 import {ENV} from "./lib/env.js";
+
 const app=express();
+const __dirname = path.resolve();
 
 console.log(process.env.PORT);
 
-app.get("/",(req,res) => {
-    res.status(200).json({msg :"Sucess from the backend"});
-});
 
-app.listen(ENV.PORT ,()=>console.log("Server is running on the port",ENV.PORT));
+// make app ready for deployment
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+const startServer = async () => {
+  try {
+    // await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
+
+startServer();
