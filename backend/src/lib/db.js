4ae8594f567e0898b786189b616982d14e600aef
mudit024 <jawaramudit1234@gmail.com>
2026-06-26 00:49:12 +0530

    DB set up

---
+++ backend/src/lib/db.js
@@ -0,0 +1,20 @@
+import dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+import mongoose from "mongoose";
+
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+  try {
+    if (!ENV.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+    console.log("✅ Connected to MongoDB:", conn.connection.host);
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB", error);
+    process.exit(1); 
+  }
+};
